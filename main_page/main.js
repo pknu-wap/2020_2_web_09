8999fad807e42f427b4a54750442447bd9cf65c5
erin-a11 <erindoh@naver.com>
2021-05-17 15:46:39 +0900

    Add main page

---
+++ main_page/main.js
@@ -0,0 +1,8 @@
+const toggleBtn = document.querySelector('.navbar_toggleBtn');
+const menu = document.querySelector('.navbar_menu');
+const icons = document.querySelector('.navbar_icons');
+
+toggleBtn.addEventListener('click', () => {
+    menu.classList.toggle('active');
+    icon.classList.toggle('active');
+});
